1c10de2949c456a132db3a2a8faf462bff2b4d82
Luis Montes <monteslu@gmail.com>
2026-02-04 08:59:20 -0700

    Add ROM header

---
+++ genesis/pong/src/boot/rom_head.c
@@ -0,0 +1,23 @@
+#include "genesis.h"
+
+__attribute__((externally_visible))
+const ROMHeader rom_header = {
+    "SEGA MEGA DRIVE ",
+    "(C)MONTESLU 2026",
+    "PONG V1.0.0                                     ",
+    "PONG V1.0.0                                     ",
+    "GM PG010000-00",
+    0x000,
+    "JD              ",
+    0x00000000,
+    0x000FFFFF,
+    0xE0FF0000,
+    0xE0FFFFFF,
+    "RA",
+    0xF820,
+    0x00200000,
+    0x0020FFFF,
+    "            ",
+    "RETRO HOMEBREW GAMES V1.0           ",
+    "JUE             "
+};
